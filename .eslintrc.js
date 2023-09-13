4d2617fc81b033f9aba0a368751cca770ae6164a
Vlad Vanca <vanca.vlad@gmail.com>
2023-09-13 14:04:35 +0300

    Upgrade to ember-cli 3.28.6

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,26 @@
 'use strict';
 
 module.exports = {
-  plugins: ['smile-ember'],
-  extends: ['plugin:smile-ember/addon'],
+  root: true,
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      legacyDecorators: true,
+    },
+  },
+  plugins: [
+    'ember'
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:ember/recommended',
+    'plugin:prettier/recommended',
+  ],
+  env: {
+    browser: true
+  },
   rules: {
     // Temporary while upgrading to Ember Octane & Glimmer components
     'ember/no-get': 'off',
@@ -18,4 +36,35 @@ module.exports = {
     // These are a lil broken right now (eslint-plugin-ember also disabled these)
     'smile-ember/order-in-components': 'off',
   },
+  ],
+  overrides: [
+    // node files
+    {
+      files: [
+        './.eslintrc.js',
+        './.prettierrc.js',
+        './.template-lintrc.js',
+        './ember-cli-build.js',
+        './index.js',
+        './testem.js',
+        './blueprints/*/index.js',
+        './config/**/*.js',
+        './tests/dummy/config/**/*.js',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+      env: {
+        browser: false,
+        node: true,
+      },
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+    },
+    {
+      // Test files:
+      files: ['tests/**/*-test.{js,ts}'],
+      extends: ['plugin:qunit/recommended'],
+    },
+  ],
 };
