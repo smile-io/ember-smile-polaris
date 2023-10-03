8b139fa5ab1e1027f44b09db8107de6bffb5d977
Vlad Vanca <vanca.vlad@gmail.com>
2023-10-03 12:40:18 +0300

    Remove useless ember-svg-jar config

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ module.exports = {
   name: require('./package').name,
 
   options: {
-    svgJar: {
-      sourceDirs: [
-        'public',
-        'tests/dummy/public/assets/images/svg',
-        'node_modules/@smile-io/ember-smile-polaris/public',
-      ],
-    },
     'ember-composable-helpers': {
       only: ['includes'],
     },
