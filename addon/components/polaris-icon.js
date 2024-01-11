5e2bd90151a9a9ec47467543a0ff0e35de5be7f1
Mariano González <mariano.gonzalez@smile.io>
2024-01-11 13:10:32 -0300

    Fix keepFills evaluation in polaris-icon

--- addon/components/polaris-icon.js
+++ addon/components/polaris-icon.js
@@ -87,7 +87,7 @@ export default class PolarisIcon extends Component.extend(SvgHandling) {
   @computed('_keepFills', 'sourcePath', 'source')
   get keepFills() {
     // If the value was previously set manually return that value
-    if (this._keepFills) {
+    if (this._keepFills !== null && this._keepFills !== undefined) {
       return this._keepFills;
     }
 
