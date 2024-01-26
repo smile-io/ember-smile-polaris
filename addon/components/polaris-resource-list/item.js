2348ac6736a1f5792f28b4a199b77e46be5b18c2
Vlad Vanca <vanca.vlad@gmail.com>
2024-01-26 14:27:11 +0200

    Fix & silence some linting issues

--- addon/components/polaris-resource-list/item.js
+++ addon/components/polaris-resource-list/item.js
@@ -1,5 +1,4 @@
 import Component from '@ember/component';
-import { readOnly } from '@ember/object/computed';
 import { action, get, computed } from '@ember/object';
 import { tagName, layout as templateLayout } from '@ember-decorators/component';
 import layout from '../../templates/components/polaris-resource-list/item';
